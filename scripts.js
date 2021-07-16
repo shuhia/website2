3292f73242e926c92d3a75560ea5b01a5759093b
Alex On <shuhia.on@gmail.com>
2021-07-16 10:12:03 +0200

    Lägg till: animation för footer

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const header = document.querySelector(".header");
 const image = document.querySelector(".img-hero");
+const footer = document.querySelector(".footer");
 
 let lastKnownScrollPosition = 0;
 let ticking = false;
@@ -12,10 +13,12 @@ document.addEventListener("scroll", function (e) {
         // On scroll down
         image.classList.add("side-image");
         header.classList.add("header-up");
+        footer.classList.add("footer-down");
       } else {
         // On scroll up
         image.classList.remove("side-image");
         header.classList.remove("header-up");
+        footer.classList.remove("footer-down");
       }
       ticking = false;
       lastKnownScrollPosition = scrollY;
